21a7079743ef4f2e07ad80f6815a5932a6385426
CarloMonroy <carlomonroy1997@gmail.com>
2022-07-30 15:42:38 -0500

    add 404 endpoint test

--- src/test_endpoint.py
+++ src/test_endpoint.py
@@ -18,3 +18,11 @@ def test_endpoint_get():
         get_response = test_client.get('/cultivos?user_id=2')
         assert get_response.status_code == 200
 
+def test_endpoint_not_found():
+    """
+    Tests not found url
+    """
+
+    with app.test_client() as test_client:
+        get_response = test_client.get('/randomurl')
+        assert get_response.status_code == 404
