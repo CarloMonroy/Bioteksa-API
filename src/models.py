c5c6d8d0e5a1d8294dd281a36c44889a8c20d742
CarloMonroy <carlomonroy1997@gmail.com>
2022-07-30 21:36:45 -0500

    Update models

--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, TIMESTAMP
+from sqlalchemy import TIME, Column, Integer, Date, BigInteger, String, TIMESTAMP
 db = SQLAlchemy()
 
 #DB Models
@@ -12,44 +12,44 @@ class users_has_cultivos(db.Model):
 
 class cultivos(db.Model):
     id = Column(Integer, primary_key=True)
-    nombre = Column()
-    ciclo_cultivo_id = Column()
-    ambiente_cultivo_id = Column()
-    fecha_inicio = Column()
-    fecha_final = Column()
-    clave_cultivo = Column()
-    creador_id = Column()
-    predios_id = Column()
-    tipos_cultivo_id = Column()
+    nombre = Column(String)
+    ciclo_cultivo_id = Column(Integer)
+    ambiente_cultivo_id = Column(Integer)
+    fecha_inicio = Column(Date)
+    fecha_final = Column(Date)
+    clave_cultivo = Column(String)
+    creador_id = Column(BigInteger)
+    predios_id = Column(BigInteger)
+    tipos_cultivo_id = Column(Integer)
 
 class cultivos_has_biodispositivos(db.Model):
     id = Column(Integer, primary_key=True)
-    cultivos_id = Column()
-    bio_dispositivos_id = Column()
-    created_at = Column()
-    updated_at = Column()
-    cultivos_id = Column()
+    cultivos_id = Column(Integer)
+    bio_dispositivos_id = Column(BigInteger)
+    created_at = Column(TIMESTAMP)
+    updated_at = Column(TIMESTAMP)
+    cultivos_id = Column(Integer)
 
 
 class bio_dispositivos(db.Model):
     id = Column(Integer, primary_key=True)
-    nombre = Column()
-    identificador = Column()
-    tipo_biodispositivos_id = Column()
-    bio_dispositivos_id = Column()
-    dispositivos_sms_id = Column()
-    clave = Column()
+    nombre = Column(String)
+    identificador = Column(String)
+    tipo_biodispositivos_id = Column(Integer)
+    bio_dispositivos_id = Column(BigInteger)
+    dispositivos_sms_id = Column(BigInteger)
+    clave = Column(String)
     updated_at = Column(TIMESTAMP)
-    created_at = Column()
-    recarga_saldo = Column()
+    created_at = Column(TIMESTAMP)
+    recarga_saldo = Column(Date)
 
 class sensores_has_bio_dispositivos(db.Model):
     id = Column(Integer, primary_key=True)
-    sensores_id = Column()
-    bio_dispositivos_id = Column()
-    updated_at = Column()
+    sensores_id = Column(BigInteger)
+    bio_dispositivos_id = Column(BigInteger)
+    updated_at = Column(TIMESTAMP)
 
 class tipo_biodispositivos(db.Model):
     id = Column(Integer, primary_key=True)
-    tipo = Column()
-    modulos = Column()
+    tipo = Column(String)
+    modulos = Column(Integer)
